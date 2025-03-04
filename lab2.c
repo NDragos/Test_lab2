bdb5aa73351f75482b294dd3a0f31e28bcf18574
NDragos <dragosneagu28@gmail.com>
2025-03-04 13:21:28 +0200

    corectat bug

--- lab2.c
+++ lab2.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 
 int function(int x){
-	x=x-2;
-	x=function(x);
+	x = x - 2;
+	if(!x)
+		x = function(x);
 	return x;
 }
 
 int main(){
-	int a=30, i=0;
+	int a = 30, i = 0;
 	
-	while (a>0){
-		a=function(a);
+	while (a > 0){
+		a = function(a);
 		i++;
 	}
 	printf("%d", i);
